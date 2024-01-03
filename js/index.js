c78610b0a20faadcc75e62dba2a2e2b718525a50
vivek9985 <vivekanando363@gmail.com>
2024-01-04 01:10:32 +0600

    added image in nav

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 const menuIcon = document.querySelector(".nav-icon");
 menuIcon.addEventListener("click", (event) => {
   gsap.to("#menu", {
-    left: 0,
-    duration: 1,
+    width: "100%",
+    duration: 0.9,
     ease: "power4.out",
   });
   gsap.from("#text", {
     y: 120,
-    duration: 0.6,
+    duration: 0.2,
     stagger: 0.1,
     ease: "power4.out",
   });
   gsap.to("#text", {
     y: 0,
-    duration: 0.6,
+    duration: 0.2,
     stagger: 0.1,
   });
 });
@@ -21,8 +21,8 @@ menuIcon.addEventListener("click", (event) => {
 const menuClose = document.querySelector(".close-menu");
 menuClose.addEventListener("click", (event) => {
   gsap.to("#menu", {
-    left: 2000,
-    duration: 1,
+    width: 0,
+    duration: 0.9,
     ease: "power4.in",
   });
 });
