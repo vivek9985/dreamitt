aa981d86dc67714cdf5553e64fc547813b28c151
vivek9985 <vivekanando363@gmail.com>
2024-01-03 23:27:50 +0600

    added animation to nav

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
 const menuIcon = document.querySelector(".nav-icon");
 menuIcon.addEventListener("click", (event) => {
   gsap.to("#menu", {
-    x: -500,
-    duration: 0.1,
+    left: 0,
+    duration: 1,
+    ease: "power4.out",
+  });
+  gsap.from("#text", {
+    y: 120,
+    duration: 0.6,
+    stagger: 0.1,
+  });
+  gsap.to("#text", {
+    y: 0,
+    duration: 0.6,
+    stagger: 0.1,
   });
 });
 
 const menuClose = document.querySelector(".close-menu");
 menuClose.addEventListener("click", (event) => {
   gsap.to("#menu", {
-    x: 0,
-    duration: 0.1,
+    left: 2000,
+    duration: 1,
+    ease: "power4.in",
   });
 });
